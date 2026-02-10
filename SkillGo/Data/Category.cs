c1a05069b8abd4a06c3f45e988a4d44df927f578
DimaPalamarchuk <dimapalamarchuk310@gmail.com>
2026-02-10 19:02:45 +0100

    Add project files.

---
+++ SkillGo/Data/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillGo.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name...")]
+        public string Name { get; set; }
+    }
+}
---
+++ SkillGo/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using SkillGo.Data;
+
+namespace SkillGo.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Task<Category> CreateAsync(Category obj);
+        public Task<Category> UpdateAsync(Category obj);
+        public Task<bool> DeleteAsync(int id);
+        public Task<IEnumerable<Category>> GetAllAsync();
+        public Task<Category> GetAsync(int id);
+    }
+}

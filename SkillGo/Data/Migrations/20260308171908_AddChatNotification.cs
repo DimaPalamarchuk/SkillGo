e1952616e925b8462ddf60a3e8410df717bf259f
DimaPalamarchuk <dimapalamarchuk310@gmail.com>
2026-03-08 22:48:08 +0100

    Design update Part 2. Bug fix

---
+++ SkillGo/Data/Migrations/20260308171908_AddChatNotification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillGo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChatNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastReadByUserAAtUtc",
+                table: "Conversations",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastReadByUserBAtUtc",
+                table: "Conversations",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastReadByUserAAtUtc",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "LastReadByUserBAtUtc",
+                table: "Conversations");
+        }
+    }
+}
--- SkillGo/Program.cs
+++ SkillGo/Program.cs
@@ -33,6 +33,15 @@ builder.Services
     .AddIdentityCore<ApplicationUser>(options =>
     {
         options.SignIn.RequireConfirmedAccount = false;
+
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+
+        options.User.RequireUniqueEmail = true;
     })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
